55ff92ef022322808bcd488ead83242db656ee73
Irina Mihaela <irinamihaela.calota@ulbsibiu.ro>
2025-07-30 02:40:49 +0300

    udpate runner script

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-use Irina\PhpDevStack\Classes\{DataTable, CsvReader, CsvWriter, SecurityManager};
+use Irina\PhpDevStack\Classes\{CsvDataTable, DataTable, CsvReader, CsvWriter, Row, SecurityManager};
 use Irina\PhpDevStack\Classes\CommandRunner;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -9,15 +9,33 @@ require_once __DIR__ . '/vendor/autoload.php';
 define('CLI_INPUT', 'php://stdin');
 define('CLI_OUTPUT', 'php://stdout');
 
+$reader = new CsvReader();
+$writer = new CsvWriter();
+$p = new CsvDataTable($reader,$writer,true);
+$p->load(CLI_INPUT);
+
+// $p->save(CLI_OUTPUT);
+//var_dump($p);
+
+
+$row = new Row();
+//$row->setHeader(['name','age']);
+//$row->setValues(['BOB',44]);
+//$row->setValues(['ana',12]);
+//$row->setValues(['mara',8]);
+//$p->addRow([1,2,5,5]);
+//$p->addRow([3,4]);
+//$/p->addRow([5,6]);
+
+$p->removeRow(1);
+$p->save(CLI_OUTPUT);
+//print_r($row->getRowWithHeaders());
+
+exit(1);
+
 $options = getopt("", [
-    "command:",
-    "headers:",
-    "column:",
-    "len:",
-    "format:",
-    "files:",
-    "pubkey:",
-    "privkey:",
+    "command:", "headers:", "column:", "len:", "format:",
+    "files:", "pubkey:", "privkey:",
 ]);
 
 $commandName = $options["command"] ?? null;
